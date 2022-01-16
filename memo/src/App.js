17cd2a17a1453130805dbcd13d7693a6b6227bcd
shreyas2-bit <62927960+shreyas2-bit@users.noreply.github.com>
2022-01-16 17:46:47 +0530

    end

--- memo/src/App.js
+++ memo/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import SingleCard from './components/SingleCard'
 
 const cardImg = [
-  { "src" : "/img/helmet-1.png"},
-  { "src" : "/img/potion-1.png"},
-  { "src" : "/img/ring-1.png"},
-  { "src" : "/img/scroll-1.png"},
-  { "src" : "/img/shield-1.png"},
-  { "src" : "/img/sword-1.png"}
+  { "src" : "/img/EZ.png",matched: false},
+  { "src" : "/img/feelsStrong.png",matched: false},
+  { "src" : "/img/sadge.png",matched: false},
+  { "src" : "/img/pepega.png",matched: false},
+  { "src" : "/img/monkaW.png",matched: false},
+  { "src" : "/img/pepehands.png",matched: false}
 ]
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
   //states for the card choices
   const[choice1,setChoice1] = useState(null)
   const[choice2,setChoice2] = useState(null)
+  const[disabled,setDisabled] = useState(false)
 
 
   //card shuffling i.e. duplication x2
@@ -28,6 +29,10 @@ function App() {
     const shuffle = [...cardImg, ...cardImg] //spreading
     .sort(() => Math.random() - 0.5 )
     .map((card) => ({...card, id: Math.random()}))
+    
+    //reset choice 1&2
+    setChoice1(null)
+    setChoice2(null)
 
     setCards(shuffle)
     setTurns(0)
@@ -42,25 +47,45 @@ function App() {
 
   //comparision
   useEffect(() => {
+    
     if(choice1 && choice2){
+      
       if(choice1.src === choice2.src){
+        setDisabled(true)
+        
         console.log("cards match")
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src===choice1.src){
+              return {...card, matched: true}
+            }else{
+              return card
+            }
+          })
+        })
         resetTurn()
       }else{
         console.log("do not match")
-        resetTurn()
+        setTimeout(() => resetTurn(), 600)
       }
     }
 
   },[choice1,choice2]) //continuesly fires the fn
 
-
+  console.log(cards)
   //choice resetus and incr
   const resetTurn = () => {
     setChoice1(null)
     setChoice2(null)
     setTurns(prevTurns => prevTurns+1 )
+    setDisabled(false)
   }
+
+  //automatically start
+  useEffect(()=> {
+    cardShuffler()
+
+  }, [])
 
   return (
     <div className="App">
@@ -74,9 +99,12 @@ function App() {
             key={card.id}
             card={card}
             handleChoice={handleChoice}
+            flipped={card===choice1 || card ===choice2 || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
 
     </div>
   );
--- memo/src/components/SingleCard.js
+++ memo/src/components/SingleCard.js
@@ -1,16 +1,16 @@
 import './singleCard.css'
 
-export default function SingleCard({card, handleChoice}) {
+export default function SingleCard({card, handleChoice, flipped, disabled}) {
     
     const handleClick = () => {
-
-        handleChoice(card)
-
+        if(!disabled){
+            handleChoice(card)
+        }
     }
     
     return (        
         <div className="card" >
-            <div>
+            <div className={flipped? "flipped":""}>
               <img className='front' src = {card.src} alt="card front"/>
               <img className='back' src="/img/cover.png" 
               onClick={handleClick}  alt="card back"/>
